3b308d78400273837fa867c4611e8a21d08a8266
João Pedro Noronha <joaopdrons@gmail.com>
2020-11-11 10:45:31 -0300

    Authentications

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '0326f7a81194c8844fed43e35b288d0c',
+    expiresIn: '1d',
+  },
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import express, { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
+import sessionsRouter from './sessions.routes';
 import usersRouter from './users.routes';
 
 const routes = Router();
@@ -7,5 +8,6 @@ routes.use(express.json());
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
