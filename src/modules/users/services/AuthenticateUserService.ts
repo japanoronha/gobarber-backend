ad4633e11f1fe0847936c89365e62492f8633185
João Pedro Noronha <joaopdrons@gmail.com>
2020-11-12 17:36:17 -0300

    Architecture and DDD

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,25 +1,31 @@
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
-import { getRepository } from 'typeorm';
-import User from '../models/User';
 
-import authConfig from '../config/auth';
-import AppError from '../errors/AppError';
+import { inject, injectable } from 'tsyringe';
 
-interface Request {
+import authConfig from '@config/auth';
+import AppError from '@shared/errors/AppError';
+import User from '../infra/typeorm/entities/User';
+import IUserRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
   email: string;
   password: string;
 }
 
-interface Response {
+interface IResponse {
   user: User;
   token: string;
 }
 
+@injectable()
 class AuthenticateUserService {
-  public async execute({ email, password }: Request): Promise<Response> {
-    const usersRepository = getRepository(User);
-    const user = await usersRepository.findOne({ where: { email } });
+  constructor(
+    @inject('UsersRepository') private usersRepository: IUserRepository,
+  ) {}
+
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
       throw new AppError('Incorrect email/password combination.', 401);
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,15 +1,19 @@
 import 'reflect-metadata';
+
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
+import AppError from '@shared/errors/AppError';
+import uploadConfig from '@config/upload';
 import routes from './routes';
-import uploadConfig from './config/upload';
 
-import './database';
-import AppError from './errors/AppError';
+import '@shared/infra/typeorm';
+import '@shared/container';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use('/', routes);
